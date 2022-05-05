8c2e7bad47ef79e9202bc6f5cacf0aa9ab130ca9
Asiern <asnulorri@gmail.com>
2022-05-05 11:46:45 +0200

    created: setup.sh

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#### Desktop ####
+
+# TODO add missing install deps
+# Install deps
+sudo pacman -S xorg xorg-server --noconfirm
+
+# Install awesomewm
+sudo pacman -S awesome --noconfirm
+
+# Install gnome
+sudo pacman -S gnome --noconfirm
+
+# Terminal deps
+sudo pacman -S lsd bat --noconfirm
+
+##### Kitty #####
+
+# Install kitty
+sudo pacman -S kitty --noconfirm
+
+
+###### ZSH ######
+
+# Install zsh
+sudo pacman -S zsh --noconfirm
+
+# Set zsh as default shell
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlightingexit
+
+# TODO Copy dotfiles
+git clone https://github.com/Asiern/dotfiles /tmp/dotfiles
+cp -r /tmp/dotfiles/awesome $(HOME)/.config/awesome 
+cp -r /tmp/dotfiles/kitty $(HOME)/.config/kitty
+cp /tmp/dotfiles/.zshrc $(HOME)/.zshrc
+rm -rf /tmp/dotfiles
